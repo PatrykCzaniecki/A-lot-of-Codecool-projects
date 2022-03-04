2e75bf282e348b9f095c1a2ac9b165e0433c22a6
PatrykCzaniecki <93118979+PatrykCzaniecki@users.noreply.github.com>
2022-03-04 12:05:04 +0100

    Add files via upload

---
+++ Ask Mate/static/js/bonusQuestions.js
@@ -0,0 +1,52 @@
+// you receive an array of objects which you must sort in the by the key "sortField" in the "sortDirection"
+function getSortedItems(items, sortField, sortDirection) {
+    console.log(items)
+    console.log(sortField)
+    console.log(sortDirection)
+
+    // === SAMPLE CODE ===
+    // if you have not changed the original html uncomment the code below to have an idea of the
+    // effect this function has on the table
+    //
+    if (sortDirection === "asc") {
+        const firstItem = items.shift()
+        if (firstItem) {
+            items.push(firstItem)
+        }
+    } else {
+        const lastItem = items.pop()
+        if (lastItem) {
+            items.push(lastItem)
+        }
+    }
+
+    return items
+}
+
+// you receive an array of objects which you must filter by all it's keys to have a value matching "filterValue"
+function getFilteredItems(items, filterValue) {
+    console.log(items)
+    console.log(filterValue)
+
+    // === SAMPLE CODE ===
+    // if you have not changed the original html uncomment the code below to have an idea of the
+    // effect this function has on the table
+    //
+    for (let i=0; i<filterValue.length; i++) {
+        items.pop()
+    }
+
+    return items
+}
+
+function toggleTheme() {
+    console.log("toggle theme")
+}
+
+function increaseFont() {
+    console.log("increaseFont")
+}
+
+function decreaseFont() {
+    console.log("decreaseFont")
+}
