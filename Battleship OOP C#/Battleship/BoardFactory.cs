9dafaa64e71813190aa6e16cb06844df3ecaf22e
PatrykCzaniecki <93118979+PatrykCzaniecki@users.noreply.github.com>
2022-05-13 14:37:09 +0200

    Add files via upload

---
+++ Battleship OOP C#/Battleship/BoardFactory.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class BoardFactory
+    {
+        public void RandomPlacement(Board boardPlayer, int size)
+        {
+            Random random = new Random();
+            int x;
+            int y;
+            List<string> directions = new List<string> {"v", "h"};
+
+            while (boardPlayer.IsAnyShipPlacementPossible())
+            {
+                x = random.Next(0, size - 1);
+                y = random.Next(0, size - 1);
+                (int, int) position = (x, y);
+                string direction = directions[random.Next(0, 2)];
+
+                if (boardPlayer.IsShipTypePlacementPossible(Square.ShipType.Carrier))
+                {
+                    boardPlayer.placeShip(position, direction, Square.ShipType.Carrier);
+                }
+                else if (boardPlayer.IsShipTypePlacementPossible(Square.ShipType.Cruiser))
+                {
+                    boardPlayer.placeShip(position, direction, Square.ShipType.Cruiser);
+                }
+                else if (boardPlayer.IsShipTypePlacementPossible(Square.ShipType.Battleship))
+                {
+                    Console.WriteLine($"bug battleship, {x}, {y}");
+                    boardPlayer.placeShip((x, y), direction, Square.ShipType.Battleship);
+                }
+                else if (boardPlayer.IsShipTypePlacementPossible(Square.ShipType.Submarine))
+                {
+                    Console.WriteLine($"bug submarine, {x}, {y}"); //bug na submarine v
+                    boardPlayer.placeShip((x, y), direction, Square.ShipType.Submarine);
+                }
+                else if (boardPlayer.IsShipTypePlacementPossible(Square.ShipType.Destroyer))
+                {
+                    Console.WriteLine($"bug destroyer, {x}, {y}"); //bug destroyer v
+                    boardPlayer.placeShip((x, y), direction, Square.ShipType.Destroyer);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        public void ManualPlacement(Board board)
+        {
+            string coordinates = "";
+            while (coordinates == "")
+            {
+                coordinates = Input.GetPlacementCoordinates();
+            }
+
+            (int, int) convertedCoordinates = (-1, -1);
+            while (convertedCoordinates == (-1,-1))
+            {
+                convertedCoordinates = Input.ConvertToCoordinates(coordinates);
+            }
+
+            string orientation = "";
+            while (orientation == "")
+            {
+                orientation = Input.GetOrientation();
+            }
+
+            Square.ShipType shipType = Square.ShipType.noSelected;
+            while (!board.IsShipTypePlacementPossible(shipType))
+            {
+                shipType = Input.GetShipType();
+            }
+            board.placeShip(convertedCoordinates,orientation,shipType);
+        }
+    }
+}
---
+++ Battleship OOP C#/Battleship/Input.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Input
+    {
+        private string move = "";
+
+        public static int ChooseMenuOptionInRange(int min, int max)
+        {
+            Console.Write("Choose option: ");
+            string user_input = Console.ReadLine();
+            if (int.TryParse(user_input, out int result))
+            {
+                if (int.Parse(user_input) >= min && int.Parse(user_input) <= max)
+                {
+                    return int.Parse(user_input);
+                }
+                else
+                {
+                    Console.WriteLine($"Input is not in specified range, from {min} to {max}");
+                    return -1;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Input is not a number!");
+                return -1;
+            }
+        }
+
+        public static string GetPlacementCoordinates()
+        {
+            Console.Write("Type coordinates: ");
+            string user_input = Console.ReadLine();
+            string VALIDLETTERS = "ABCDEFGHIJ"; 
+            string VALIDNUMBERS = "12345678910";
+            if (user_input.Length == 2 && VALIDLETTERS.Contains(user_input.Substring(0, 1).ToUpper()) && 
+                VALIDNUMBERS.Contains(user_input.Substring(1, 1).ToString().ToUpper()))
+            {
+                return user_input.ToUpper();
+            }
+            else
+            {
+                Console.WriteLine("Invalid coordinates!");
+                return "";
+            }
+        }
+
+        public static (int, int) ConvertToCoordinates(string coordinates)
+        {
+            string xString = coordinates.Substring(0, 1).ToString().ToUpper();
+            int x;
+            switch (xString)
+            {
+                case "A":
+                    x = 0;
+                    break;
+                case "B":
+                    x = 1;
+                    break;
+                case "C":
+                    x = 2;
+                    break;
+                case "D":
+                    x = 3;
+                    break;
+                case "E":
+                    x = 4;
+                    break;
+                case "F":
+                    x = 5;
+                    break;
+                case "G":
+                    x = 6;
+                    break;
+                case "H":
+                    x = 7;
+                    break;
+                case "I":
+                    x = 8;
+                    break;
+                default:
+                    x = 9;
+                    break;
+            }
+            int y = int.Parse(coordinates.Substring(1, 1))-1;
+            return (x, y);
+        }
+
+        public static string GetOrientation()
+        {
+            Display.Orientation();
+            string user_input = Console.ReadLine();
+            string VALIDLETTERS = "HV";
+            if (user_input.Length == 1 && VALIDLETTERS.Contains(user_input.Substring(0, 1).ToUpper()))
+            {
+                return user_input.ToUpper();
+            }
+            else
+            {
+                Console.WriteLine("Invalid orientation!");
+                return "";
+            }
+            throw new NotImplementedException();
+        }
+
+        public static Square.ShipType GetShipType()
+        {
+            int user_input = -1;
+            while (user_input == -1)
+            {
+                Display.ShipTypes();
+                user_input = ChooseMenuOptionInRange(1, 5);
+            }
+
+            Square.ShipType shipType;
+
+            switch (user_input)
+            {
+                case 1:
+                    shipType = Square.ShipType.Carrier;
+                    break;
+                case 2:
+                    shipType = Square.ShipType.Cruiser;
+                    break;
+                case 3:
+                    shipType = Square.ShipType.Battleship;
+                    break;
+                case 4:
+                    shipType = Square.ShipType.Submarine;
+                    break;
+                default:
+                    shipType = Square.ShipType.Destroyer;
+                    break;
+            }
+            return shipType;
+        }
+    }
+}
