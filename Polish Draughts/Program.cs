70a930f29d5b96614b4883efbd76c430a9715a37
PatrykCzaniecki <93118979+PatrykCzaniecki@users.noreply.github.com>
2022-09-09 13:13:13 +0200

    Add files via upload

---
+++ Polish Draughts/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace Draughts
+{
+    public class Game
+    {
+        public static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("Welcome Player");
+            Console.WriteLine("What size of board u choose(10-20):");
+            var size = Console.ReadLine();
+            Round(Int32.Parse(size));
+        }
+
+        public static void Round(int size)
+        {
+            Board board = new Board(size);
+            board.DisplayBoard();
+            board.Move();
+            board.DisplayBoard();
+        }
+    }
+}
