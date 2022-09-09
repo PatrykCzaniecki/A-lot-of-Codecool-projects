d860dca481bf1acd0227a28664b5c935b20ce8b7
PatrykCzaniecki <93118979+PatrykCzaniecki@users.noreply.github.com>
2022-09-09 13:08:35 +0200

    Add files via upload

---
+++ CodecoolShop/Data/Migrations/20220704094718_Product_description_added.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class Product_description_added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+        }
+    }
+}
---
+++ CodecoolShop/Domain/Supplier.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain;
+
+public class Supplier
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+}
